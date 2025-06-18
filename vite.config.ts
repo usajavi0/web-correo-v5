6d6a7288d841245885632072e9f77dc80ed0f763
usajavi0 <tu-correo-real@dominio.com>
2025-06-18 13:46:43 +0200

    chore: Change build output to docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Le dice a tu web que vivirá en la subcarpeta /web-correo-v5/ en GitHub
+
+  // Le dice a tu web que vivirá en la subcarpeta /web-correo-v5/ en GitHub.
   base: '/web-correo-v5/',
+
+  // Especifica que la carpeta de salida para la compilación será 'docs'
+  build: {
+    outDir: 'docs'
+  }
 })
