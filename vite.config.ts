0986e274d6102a070c5a1fb97cccebb796277316
usajavi0 <tu-correo-real@dominio.com>
2025-06-18 12:46:41 +0200

    Alineación final. El Despacho.

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,9 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react'; // Importamos el plugin de React
+    import { defineConfig } from 'vite'
+    import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-        // --- NUESTRAS ADICIONES ---
-        plugins: [react()],          // 1. Añadimos el plugin de React
-        base: '/web-correo-v5/',   // 2. Añadimos la ruta para GitHub Pages
-
-        // --- TU CONFIGURACIÓN ORIGINAL (LA MANTENEMOS) ---
-        define: {
-            'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-        },
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, '.'),
-            }
-        }
-    };
-});
+    // https://vitejs.dev/config/
+    export default defineConfig({
+      plugins: [react()],
+      base: '/web-correo-v5/', // ¡Esta es la línea clave!
+    })
+    
